4949f1e7497901c9ccc4265baac5bbaa5a243d7a
Bailey Wood <woodbmae@gmail.com>
2019-10-21 11:46:17 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        Show.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        Show.where(rating: highest_rating).first #why do you need first?
+    end
+
+    def self.lowest_rating
+        Show.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        Show.where(rating: lowest_rating).first
+    end
+
+    def self.ratings_sum
+        Show.sum(:rating)
+    end
+
+    def self.popular_shows
+        Show.select do |show|
+            show.rating > 5
+        end
+    end
+
+    def self.shows_by_alphabetical_order
+        Show.order('name asc')
+    end
+
+end
